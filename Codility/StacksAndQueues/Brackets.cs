d74650c3ac57b6698befc4fcbdc6d4dc9886287d
asmodat <asmodat@hotmail.com>
2017-02-06 15:14:18 +0100

    2017-02-06

---
+++ Codility/StacksAndQueues/Brackets.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using static System.Console;
+
+namespace Codility.StacksAndQueues
+{
+    public class Brackets
+    {
+
+        
+        public static int solution(int[] A)
+        {
+          
+            return -1;
+        }
+
+        
+    }
+}
+
+/*
+
+     */
